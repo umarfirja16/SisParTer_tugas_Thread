728da26137fd7674c97d12d31b314c2c62b947d2
muthianovisy <63630343+muthianovisy@users.noreply.github.com>
2020-04-17 13:49:57 +0700

    Fix 02.server_thread.py

--- 02.server_thread.py
+++ 02.server_thread.py
@@ -1,5 +1,8 @@
 # import socket, sys, traceback dan threading
-
+import threading
+import socket
+import sys
+import traceback
 
 # jalankan server
 def main():
@@ -8,13 +11,13 @@ def main():
 # fungsi saat server dijalankan
 def start_server():
     # tentukan IP server
-
+    ip = "127.0.0.1"
     
     # tentukan port server
-
+    port = 12345
 
     # buat socket bertipe TCP
-
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     # option socket
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -22,70 +25,72 @@ def start_server():
 
     # lakukan bind
     try:
-        
+        sock.bind((ip, port))
     except:
         # exit pada saat error
         print("Bind gagal. Error : " + str(sys.exc_info()))
         sys.exit()
 
     # listen hingga 5 antrian
-    
+    sock.listen(5)
     print("Socket mendengarkan")
 
     # infinite loop, jangan reset setiap ada request
     while True:
         # terima koneksi
-        
+        conn, addr = sock.accept()
         
         # dapatkan IP dan port
         
-        print("Connected dengan " + ip + ":" + port)
+        print("Connected dengan " + str(ip) + ":" + str(port))
 
         # jalankan thread untuk setiap koneksi yang terhubung
         try:
-            
+            c = threading.Thread(target=client_thread,args=(conn,addr[0],addr[1],4096))
+            c.start()
         except:
             # print kesalahan jika thread tidak berhasil dijalankan
             print("Thread tidak berjalan.")
             traceback.print_exc()
 
     # tutup socket
-    
+    sock.close()
 
 
 def client_thread(connection, ip, port, max_buffer_size = 4096):
     # flag koneksi
-    
+    is_active = True
 
     # selama koneksi aktif
     while is_active:
 
         # terima pesan dari client
-        
+        data = connection.recv( max_buffer_size)
         
         # dapatkan ukuran pesan
-        
+        client_input_size = sys.getsizeof(data)
         
         # print jika pesan terlalu besar
         if client_input_size > max_buffer_size:
             print("The input size is greater than expected {}")
 
         # dapatkan pesan setelah didecode
-        
+        client_input=data.decode()
         
         # jika "quit" maka flag koneksi = false, matikan koneksi
         if "quit" in client_input:
             # ubah flag
-        
+            is_active = False
             print("Client meminta keluar")
             
             # matikan koneksi
-            
-            print("Connection " + ip + ":" + port + " ditutup")
+            connection.close()
+            print("Connection " + str(ip) + ":" + str(port) + " ditutup")
             
         else:
             # tampilkan pesan dari client
-            
+            print(client_input)
+
             
 # panggil fungsi utama
 if __name__ == "__main__":
